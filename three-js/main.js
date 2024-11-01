42d58ad6fcaeda88f612ba0d70427eb271f0f726
Brynner03 <bventura03@hotmail.com>
2024-11-01 13:10:11 -0400

    Moved code for an animated cube to a separate file, and created a new animated blue line.

--- three-js/main.js
+++ three-js/main.js
@@ -1,23 +1,35 @@
 import * as THREE from 'three'
 
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+// Code to draw lines
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const scene = new THREE.Scene();
 
-camera.position.z = 5;
+// Create a blue LineBasicMaterial
+const material = new THREE.LineBasicMaterial( {color: 0x0000ff } );
 
+const points = [];
+points.push( new THREE.Vector3 ( -10, 0, 0 ) );
+points.push( new THREE.Vector3 ( 0, 10, 0 ) );
+points.push( new THREE.Vector3 ( 10, 0, 0 ) );
+
+const geometry = new THREE.BufferGeometry().setFromPoints( points )
+
+// Put points and material together to make a line
+const line = new THREE.Line( geometry, material )
+
+scene.add(line)
+renderer.render( scene, camera )
 
 function animate() {
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+  line.rotation.x += 0.02
+  line.rotation.y -= 0.03
 	renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
